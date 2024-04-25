398b9ee84fbadd98a6da34f12ed136dd3a61ff44
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2024-04-25 09:18:41 -0500

    Add files via upload

---
+++ Kafka_admin.py
@@ -0,0 +1,15 @@
+    # cd kafka_2.12-3.5.1
+
+    # Install the kafka-python package by running the following command.
+
+    # pip3 install kafka-python
+
+    # Create a new file named admin.py by running the following command.
+
+    # touch admin.py
+from kafka.admin import KafkaAdminClient,NewTopic
+admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092", client_id='test')
+topic_list = []
+new_topic = NewTopic(name="bankbranch", num_partitions= 2, replication_factor=1)
+topic_list.append(new_topic)
+admin_client.create_topics(new_topics=topic_list)
